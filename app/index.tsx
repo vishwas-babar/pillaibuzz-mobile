77c8347beaa80107b89620649a7a1147bc077b7d
vishwas-babar <vishwasbabar886@gmail.com>
2026-01-29 23:09:20 +0530

    Setup Expo Router with NativeWind and Tailwind v3

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Home() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-600">
+      <Text className="text-white text-xl">
+        PillaiBuzz Mobile Working 🚀
+        PillaiBuzz Mobile Working 🚀
+        PillaiBuzz Mobile Working 🚀
+        PillaiBuzz Mobile Working 🚀
+        PillaiBuzz Mobile Working 🚀
+        PillaiBuzz Mobile Working 🚀
+      </Text>
+    </View>
+  );
+}
